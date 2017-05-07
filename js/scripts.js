19b9609579808908c9c0f423e1322fd873c15f93
Danielle Roberson <danielle.roberson@icloud.com>
2017-05-06 22:49:12 -0700

    .

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,7 @@
 // @codekit-prepend "semantic.js";
 
 console.log('Hello, World!');
+
+  const addItems = document.querySelector('.add-items');
+  const itemsList = document.querySelector('.plates');
+  const items = [];
